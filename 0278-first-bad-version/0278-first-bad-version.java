b96fcb29d3e06a0031b429c8b356fdd14385c503
leesumin <45748683+leelee31@users.noreply.github.com>
2023-01-06 14:08:07 +0900

    Time: 19 ms (66.01%), Space: 40.8 MB (37.00%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -4,19 +4,14 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1, right = n;
-        int mid = 0;
-        while (left <= right) {
+        int mid = n;
+        while (left < right) {
             mid = left + (right - left)/2;
-            if (isBadVersion(mid) && (mid == right || mid == left)) 
-                return mid;
-            if (!isBadVersion(mid) && isBadVersion(mid+1))
-                return mid + 1;
-            
             if (isBadVersion(mid))
-                right = mid - 1;
+                right = mid;
             else
                 left = mid + 1;
         }
-        return n;
+        return right;
     }
 }
