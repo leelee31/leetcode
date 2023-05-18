bd46cd78c7033d18fbdfd84a26536fcf11b2f906
leesumin <45748683+leelee31@users.noreply.github.com>
2023-05-18 13:09:57 +0900

    Time: 5 ms (94.72%), Space: 42 MB (85.70%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,46 +1,22 @@
-import java.math.BigInteger;
-
-class Solution {
+public class Solution {
     public String largestNumber(int[] nums) {
-        sort(nums, 0, nums.length-1);
-        StringBuilder sb = new StringBuilder();
-        if (nums[0] == 0)
-            sb.append("0");
-        else 
-            for (int n : nums)
-                sb.append(n+"");
-        return sb.toString();
-    }
-    
-    public void sort(int[] nums, int start, int end) {
-        for (int i=start; i<end; i++) {
-            for (int j=i+1; j<end+1; j++) {
-                if (findFrontNum(nums[i]) < findFrontNum(nums[j])) {
-                    swap(nums, i, j);
-                }
-                if (findFrontNum(nums[i]) == findFrontNum(nums[j])) {
-                    BigInteger n1 = new BigInteger(nums[i] + "" + nums[j]);
-                    BigInteger n2 = new BigInteger(nums[j] + "" + nums[i]);
-                    if (n1.compareTo(n2) == -1) {
-                        swap(nums, i, j);
-                    }
-                }
-            }
+        if (nums == null || nums.length == 0) 
+            return "";
+        
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
         }
-    }
-    
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    public int findFrontNum(int num) {
-        if (num < 10) return num;
-        int result = num;
-        while (result / 10 != 0) {
-            result /= 10;
+
+        Arrays.sort(strs, (s1, s2) -> (s2+s1).compareTo(s1+s2));
+        
+        if (strs[0].charAt(0) == '0') 
+            return "0";
+        
+        StringBuilder answer = new StringBuilder();
+        for (String s : strs) {
+            answer.append(s);
         }
-        return result;
+        return answer.toString();
     }
 }
