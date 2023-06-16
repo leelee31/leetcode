6eb5aadf39d8a8b262824961587a1a891a8c1bfc
leesumin <45748683+leelee31@users.noreply.github.com>
2023-06-16 10:48:42 +0900

    Time: 5 ms (97.16%), Space: 44.4 MB (38.97%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -2,27 +2,26 @@ class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         int[] answer = new int[queries.length];
         Arrays.sort(nums);
+        for (int i=1; i<nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
         for (int i=0; i<queries.length; i++) {
-            if (nums[0] > queries[i]) continue;
-            int s=0, e=0, count=0, sum=0;
-            while (s <= e && e < nums.length && nums[e] <= queries[i]) {
-                if (nums[e] + sum > queries[i]) {
-                    sum = 0;
-                    s++;
-                    e = s;
-                } else if (nums[e] + sum < queries[i]) {
-                    count = Math.max(count, e - s + 1);
-                    sum += nums[e];
-                    e++;
-                } else {
-                    count = Math.max(count, e - s + 1);
-                    sum = 0;
-                    s++;
-                    e = s;
-                }
-            }
-            answer[i] = count;
+            answer[i] = binarysearch(nums, queries[i]);
         }
         return answer;
+    }
+    
+    private int binarysearch(int nums[], int target){
+        int s=0, e=nums.length-1, len = 0;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            if(nums[mid] <= target) {
+                len = mid + 1;
+                s = len;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return len;
     }
 }
